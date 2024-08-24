4ee45c1ba8abe53033eb0548f45a1401b020b48a
cschot <40297097+cschot@users.noreply.github.com>
2024-08-24 17:15:19 +0000

    Update test_update.py

--- tests/test_update.py
+++ tests/test_update.py
@@ -1,8 +1,12 @@
 from tools.operations import update_gtfs_schedule_source
+from pathlib import Path
 
 def test_catalogs_sources_gtfs_schedule_source_ids_uniqueness():
     update_gtfs_schedule_source(
         mdb_source_id=1159,
         provider="testSTER",
     )
+    filepath = "catalogs/sources/gtfs/schedule/fr-ile-de-france-star-gtfs-1159.json"
+    contents = Path(file_path).read_text()
+    print(contents)
     assert 1
