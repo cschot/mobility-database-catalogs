8e3dbd830360e9bf22a16f1010be974e0dba9840
cschot <40297097+cschot@users.noreply.github.com>
2024-08-24 18:14:02 +0000

    Update test_update.py

--- tests/test_update.py
+++ tests/test_update.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def test_catalogs_sources_gtfs_schedule_source_ids_uniqueness():
     update_gtfs_schedule_source(
         mdb_source_id=1159,
-        provider="testSTER",
+        direct_download="https://eu.ftp.opendatasoft.com/star/gtfs/GTFS_1_20240820_20240901_20240819145156.zip",
     )
     file_path = "catalogs/sources/gtfs/schedule/fr-ile-de-france-star-gtfs-1159.json"
     contents = Path(file_path).read_text()
